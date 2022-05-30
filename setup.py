9e045f6631cb878ee16638502c5022e68d2cc283
ysoh <ysoh@kalman.kr>
2022-05-31 00:44:42 +0900

    fix recv handler

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open ("README.md", "r") as f:
 
 setuptools.setup(
     name="pcan-gatewaybus",
-    version="0.0.1",
+    version="0.0.2",
     author="yunsik oh",
     author_email="oyster90@naver.com",
     description="pcan gatewaybus backend for \"python-can\"",
